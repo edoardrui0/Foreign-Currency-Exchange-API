8fc8ed81c7e599835b1f78841a59265db00f21f8
Edoardo Ruiz <edoardrui0@gmail.com>
2020-11-12 17:28:22 -0500

    added fetch and WatchForm functions, updated HTML to show currencies

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 "use strict";
+
+var myHeaders = new Headers();
+myHeaders.append(
+  "Cookie",
+  "__cfduid=dece6c742836bb1559d13c983611d05531605215826"
+);
+
+var requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow",
+};
+
+let apiKey = "829db5a4a4e6684ed3818e72c0c8a7bd";
+
+function getCurrencies(currentCurrency, foreignCurrency, currencyAmount) {
+  fetch(
+    `http://data.fixer.io/api/convert?access_key=${apiKey}&from=${currentCurrency}&to=${foreignCurrency}&amount=${currencyAmount}`,
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((responseJson) => console.log(responseJson))
+    .catch((error) => console.log("error" + error));
+}
+
+function watchForm() {
+  $("form").submit((event) => {
+    event.preventDefault();
+    let currentCurrency = $('select[name="currenctCurrencyName"]')
+      .val()
+      .toUpperCase();
+    let foreignCurrency = $('select[name="foreignCurrencyName"]')
+      .val()
+      .toUpperCase();
+    let currencyAmount = $('input[name="currencyAmount"]').val();
+    getCurrencies(currentCurrency, foreignCurrency, currencyAmount);
+  });
+}
+
+$(function () {
+  console.log("App loaded! Waiting for submit!");
+  watchForm();
+});
